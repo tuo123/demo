a80bfaf532ba29f443482a80463e1d9d03e58bd0
JavaDemo <865077142@qq.com>
2019-03-26 13:13:10 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/JavaController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tuo
+ * @create 2019-03-26 下午 12:43
+ */
+@RestController
+public class JavaController {
+
+    @GetMapping(value = "test")
+    public String test(){
+        return "这是一个测试demo";
+    }
+}
